84e047373e7266aabb54a9d34c696d69e7467037
Ten Cua Ban <email_cua_ban@example.com>
2026-08-13 01:51:09 +0700

    fix staff workflow data and history

--- src/main/java/com/shinecraft/server/booking/BookingRepository.java
+++ src/main/java/com/shinecraft/server/booking/BookingRepository.java
@@ -19,5 +19,8 @@ public interface BookingRepository extends JpaRepository<Booking, Long> {
     @EntityGraph(attributePaths = {"customer", "vehicle", "services"})
     List<Booking> findByStatusInOrderByScheduledAtAsc(List<BookingStatus> statuses);
 
+    @EntityGraph(attributePaths = {"assignedStaff"})
+    List<Booking> findByAssignedStaffIsNotNull();
+
     boolean existsByScheduledAtAndStatusIn(LocalDateTime scheduledAt, List<BookingStatus> statuses);
 }
